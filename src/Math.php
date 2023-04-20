7a53a9b28418571bcff928fc2736ab7437e13fc4
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2023-04-20 11:58:08 -0300

    Se preparan test para distribir hasta con billetes de cien

--- src/Math.php
+++ src/Math.php
@@ -6,59 +6,120 @@ class Math
     /**
      * Distribuye un valor entre distintas denominaciones
      * 
-     * @param number     $valor   : Que se desea revisar
-     * @param array(int) $monedas : lista con las denominacion de monedas que se pueden utilizar
+     * @param number     $valor_original   : Que se desea revisar
+     * @param array(int) $monedas_original : lista con las monedas
      * 
      * @return array
      */
-    public function devolverCambio($valor = 0, $monedas = array())
+    public function devolverCambio($valor_original = 0, $monedas_original = array())
     {
-        // Multiplica el valor y las monedas por 100 para trabajar con enteros
-        $valor = intval($valor * 100);
+        $valor = intval($valor_original * 100);
         
-        foreach ($monedas as &$moneda) {
-            $moneda = intval($moneda * 100);
+        $monedas=array();
+        foreach ($monedas_original as $moneda) {
+             $monedas[] = intval($moneda * 100);
         }
-
+        
         // Ordena las monedas en orden descendente
-        rsort($monedas, SORT_NUMERIC);
-        // print_r($monedas);
+        rsort($monedas);
 
         // Inicializa el array de resultados
         $resultados = array();
 
+        
         // Itera a través de cada moneda y calcula la cantidad necesaria
-        $iteracion = 0;
         foreach ($monedas as $moneda) {
-            $iteracion++;
-            printf("\n %02s %6s %06s", $iteracion, $moneda, $valor);
+
             // Si la moneda es mayor que el valor, no puede ser utilizada
+            // echo "\n[$key ($moneda > $valor)]".(($moneda > $valor)?"T":"F");
             if ($moneda > $valor) {
                 continue;
             }
 
             // Calcula la cantidad de monedas necesarias
-            $cantidad = floor($valor / $moneda);
-            // echo "($valor / $moneda) = $cantidad";
+            $cantidad = (floor($valor / $moneda));
+            // echo "\n\t $cantidad = floor($valor / $moneda))";
 
             // Almacena la cantidad de monedas necesarias
-            // $resultados["$moneda"] = $cantidad;
-            
-            $resultados[number_format($moneda / 100, 2)] = $cantidad;
+            $index = number_format($moneda / 100, 2, '.', '');
+            $resultados[$index] = $cantidad;
 
             // Resta el valor de las monedas utilizadas
             $valor -= $cantidad * $moneda;
         }
-    
 
-        // Si todavía hay un valor restante, no se puede devolver el cambio exacto
+        // // Si todavía hay un valor restante, no se puede devolver el cambio exacto
         // if ($valor > 0) {
         //     return false;
         // }
 
         // Devuelve el array de resultados
-        return array_map("intval", $resultados);
+        return array_map('intval', $resultados);
     }
+
+    /**
+     * Distribuye un valor entre distintas denominaciones
+     * 
+     * @param number     $valor_original   : Que se desea revisar
+     * @param array(int) $monedas_original : lista con las monedas
+     * @param number     $limite_original  : tope para generar un ticket vuelto
+     * 
+     * @return array
+     */
+    public function devolverCambioRestringido($valor_original = 0, $monedas_original = array(), $limite_original = 0)
+    {
+        $valor = intval($valor_original * 100);
+        $limite = intval($limite_original * 100);
+        
+        // quito to esa moneda
+
+
+        $monedas=array();
+        foreach ($monedas_original as $moneda) {
+             $monedas[] = intval($moneda * 100);
+        }
+        
+        // Ordena las monedas en orden descendente
+        rsort($monedas);
+
+        // Inicializa el array de resultados
+        $resultados = array();
+
+        
+        // Itera a través de cada moneda y calcula la cantidad necesaria
+        foreach ($monedas as $moneda) {
+
+            // Si la moneda es mayor que el valor, no puede ser utilizada
+            // echo "\n[$key ($moneda > $valor)]".(($moneda > $valor)?"T":"F");
+            if ($moneda > $valor) {
+                continue;
+            }
+
+            // Calcula la cantidad de monedas necesarias
+            $cantidad = (floor($valor / $moneda));
+            // echo "\n\t $cantidad = floor($valor / $moneda))";
+
+            // Almacena la cantidad de monedas necesarias
+            $index = number_format($moneda / 100, 2, '.', '');
+            $resultados[$index] = $cantidad;
+
+            // Resta el valor de las monedas utilizadas
+            $valor -= $cantidad * $moneda;
+        }
+
+        // // Si todavía hay un valor restante, no se puede devolver el cambio exacto
+        // if ($valor > 0) {
+        //     return false;
+        // }
+
+        // Devuelve el array de resultados
+        return array(
+                'resultados'=> array_map('intval', $resultados),
+                'resto' => number_format($valor / 100, 2)
+        );
+    }
+
+
 
     public function fibonacci($n)
     {
